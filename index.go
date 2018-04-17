9b202bef4d41e5bce3a52b0215e96293fc552c67
Ryan Loader <rani_sputnik@hotmail.com>
2018-04-17 01:28:24 +0100

    Jazz up index.html

--- index.go
+++ index.go
@@ -18,13 +18,58 @@ const indexHTML = `<!doctype html>
 		<link rel="apple-touch-icon" href="icon.png">
 		<!-- Place favicon.ico in the root directory -->
 
-		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+		<style>
+			.container {
+				max-width: 520px;
+			}
+
+			.footer {
+				position: absolute;
+				bottom: 0;
+				width: 100%;
+				height: 60px;
+				line-height: 60px;
+				background-color: #f5f5f5;
+			}
+		</style>
 	</head>
 	<body>
-		<form enctype="multipart/form-data" action="/build" method="POST">
-			<input type="file" name="uploadfile" accept=".love"/>
-			<input type="submit" value="upload" />
-		</form>
+		<div class="container text-center">
+			</br><h1>LÖVE Distributor</h1></br>
+			
+			<p class="lead">Build and distribute your <a href="https://love2d.org/" target="_blank">LÖVE</a> games easily.</p>
+			</br>
+
+			<div class="card">
+				<div class="card-body">
+					<form class="form-inline" enctype="multipart/form-data" action="/build" method="POST">
+						<div class="form-group">
+							<input type="file" class="form-control-file" id="fileInput" name="uploadfile" accept=".love" required>
+						</div>
+						<button class="btn btn-primary" type="submit">Upload</button>
+					</form>
+				</div>
+			</div>
+
+			</br>
+
+			<div class="card">
+				<h5 class="card-header">How it works</h5>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">Upload your .love file using the form above.</li>
+					<li class="list-group-item">We'll send you executables for Windows, Mac and Linux.</li>
+					<li class="list-group-item">Download and play the game or share with friends.</li>
+				</ul>
+			</div>
+		</div>
+
+		<footer class="footer">
+			<div class="container text-center">
+				<span class="text-muted"><small>Made with LÖVE by <a href="http://ryanloader.me">Ryan Loader<a/></small></span>
+			</div>
+		</footer>
 	</body>
 </html>
 `
