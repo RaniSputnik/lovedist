4a4551cf26c17be140ebf851602963786ebb3e1c
Ryan Loader <talk@ryanloader.me>
2019-11-24 16:35:15 +0000

    Copy over existing osx builder

--- dist/builder.go
+++ dist/builder.go
@@ -1,35 +1,52 @@
 package dist
 
-type Output struct {
+import (
+	"errors"
+	"io"
+)
+
+type Project struct {
+	Name string
+	// BundleID is typically a string conveying domain
+	// ownership eg. com.example.GameName.
+	BundleID string
+}
+
+type Result struct {
 	/* TODO: What is this? */
 }
 
 type Builder interface {
-	// Build accepts an io.Reader that will read the *.love
-	// file containing the games assets. Build returns the
-	// result of the build and an error if the build was
-	// unsuccessful for any reason.
-	Build(input io.Reader) (Output, error)
+	// Build generates a compiled Love2d game.
+	//
+	// Build accepts two arguments, a project struct containing
+	// information about the project being compiled and an
+	// io.Reader that will read the *.love file containing
+	// the games assets.
+	//
+	// Build returns the result of the build and an error
+	// if the build was unsuccessful for any reason.
+	Build(p Project, input io.Reader) (Result, error)
 }
 
-type notImplementedBuilder struct {}
+type notImplementedBuilder struct{}
 
-func (notImplementedBuilder) Build(input io.Reader) (res Output, err error) {
+func (notImplementedBuilder) Build(p Project, input io.Reader) (res Result, err error) {
 	return res, errors.New("not implemented")
 }
 
-// Win returns a builder that can build Love games for 
+// Win returns a builder that can build Love games for
 // distribution on Windows PCs
-func Win() Builder { return notImplementedBuilder{} } 
+func Win() Builder { return notImplementedBuilder{} }
 
 // OSX returns a builder that can build Love games for
 // distribution on desktop Mac.
 func OSX() Builder { return notImplementedBuilder{} }
 
-// Linux returns a builder that can build Love games for 
+// Linux returns a builder that can build Love games for
 // distribution on Linux computers.
 func Linux() Builder { return notImplementedBuilder{} }
 
-// Web returns a builder that can build Love games for 
+// Web returns a builder that can build Love games for
 // distribution on the web using web assembly.
 func Web() Builder { return notImplementedBuilder{} }
